8df1b9cdfe5ed87c44d07c5343b70e019035e530
Nitin <bandooknitin@gmail.com>
2025-02-10 18:37:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,22 +8,31 @@ const messages = [
     "Last chance to change your mind! 🤞",
     "Okay, I give up... (not really) 😜",
     "You're so stubborn! 😤",
-    "Fine, I still love you anyway! ❤️"
+    "Fine, I still love you anyway! ❤️",
+    "Alright, this is the last time I'm asking...",
+    "Wait... the 'No' button is disappearing! 😳",
+    "Oops, looks like you have no choice now! 😆"
 ];
-
 
 let messageIndex = 0;
 
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
-    
-    noButton.textContent = messages[messageIndex];
-    messageIndex = (messageIndex + 1) % messages.length;
+
+    if (messageIndex < messages.length) {
+        noButton.textContent = messages[messageIndex];
+        messageIndex++;
+    }
 
     // Increase "Yes" button size
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.3}px`;
+    yesButton.style.fontSize = `${currentSize * 1.2}px`;
+
+    // Hide the "No" button after the last message
+    if (messageIndex >= messages.length) {
+        noButton.style.display = "none";
+    }
 }
 
 function handleYesClick() {
